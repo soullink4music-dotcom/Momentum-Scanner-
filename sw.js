3819cefcd289508f16c791832cb3f0de5f627672
soullink4music-dotcom <soullink4music@gmail.com>
2026-07-08 07:41:31 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", e => self.skipWaiting());
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => {
+    return new Response("Offline");
+  }));
+});
